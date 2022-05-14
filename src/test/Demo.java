b16f8b47105cddc8d548ebcb0edb70361b60136f
Shubham <Shubham@LAPTOP-N4CSOG78>
2022-05-14 13:24:29 +0530

    added a class demo in which some code is written

---
+++ src/test/Demo.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Git demo on 14th May");
+		
+		System.out.println("line 2 after add");
+	}
+	
+}
